e3981bbda3ebdd344137539685185067cb97b42c
Nikolay Kuchumov <kuchumovn@gmail.com>
2012-02-23 18:21:49 +0400

    added width check

--- jquery-full-house.js
+++ jquery-full-house.js
@@ -9,11 +9,11 @@
 
 /* html:
 
-<div class="fill_with_text">This</div>
+<div class="fill_with_text little">This</div>
 
-<div class="fill_with_text">Lorum ipsum dolor sit amet</div>
+<div class="fill_with_text little">Lorum ipsum dolor sit amet</div>
 
-<div class="fill_with_text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>​
+<div class="fill_with_text much">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>​
 */
 
 /* css:
@@ -46,7 +46,8 @@ $(function()
     function find_max_font_size(container)
     {
         var text = container.text()
-        var available_height = container.height()
+        var available_height = container.outerHeight()
+        var available_width = container.outerWidth()
         
         container.empty()
             
@@ -63,9 +64,12 @@ $(function()
             try_font_size(font_size)
         
             var current_height = container[0].scrollHeight
+            var current_width = container[0].scrollWidth
+			
             var height_proportion = current_height / available_height
+            var width_proportion = current_width / available_width
             
-            if (height_proportion > 1)
+            if (height_proportion > 1 || width_proportion > 1)
                 return font_size - 1
             else
                 return find_max_font_size_starting_with(font_size + 1)
@@ -76,12 +80,19 @@ $(function()
         return font_size
     }
     
-    $.fn.fill_with_text = function()
+    $.fn.fill_with_text = function(options)
     {
         return $(this).each(function()
         {
             var container = $(this)
             container.css({ fontSize: find_max_font_size(container) + unit_of_measurement })
+			
+			if (options)
+				if (options['center vertically'])
+				{
+					var label = $('<span/>').text(container.text()).appendTo(container.empty())
+					label.css({ position: 'relative', top: parseInt((container.height() - label.height()) / 2) + 'px' })
+				}
         })
     }
 })(jQuery)
