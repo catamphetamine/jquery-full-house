4ad4291df122c8cd1267e6be9003c7e22c5a1ce5
Nikolay Kuchumov <kuchumovn@gmail.com>
2012-05-04 19:09:01 +0400

    algorythms refactored, test page refined

--- jquery-full-house.js
+++ jquery-full-house.js
@@ -2,46 +2,89 @@
     Based on this script by Marcus Ekwall
     http://jsfiddle.net/mekwall/fNyHs/
 
+	Examples, support and the newest version of this script is here:
+	https://github.com/kuchumovn/jquery-full-house
+
     Author: Nikolay Kuchumov
     github: kuchumovn
     email: kuchumovn@gmail.com
 */
 
-/* html:
-
-<div class="fill_with_text little">This</div>
-
-<div class="fill_with_text little">Lorum ipsum dolor sit amet</div>
-
-<div class="fill_with_text much">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>​
-*/
-
-/* css:
-
-.fill_with_text {
-    width: 530px;
-    height: 240px;
-    font-size: 10px;
-    text-align: center;
-    border: 1px solid #ccc;
-    margin: 10px;
-    overflow: hidden;
-}
-*/
-
-/* javascript:
-
-$(function()
-{
-    $(".fill_with_text.little").fill_with_text()
-    
-    $(".fill_with_text.much").fill_with_text()
-})​​
-*/
-
 (function($)
 {
-	var Font_size_increment_step = 10
+	var Algorythm = 
+	{
+		Interface: function(options)
+		{
+			this.too_big = function(x) {}
+			this.fits = function(x) {}			
+			// this.retry function will be set automatically
+		},
+	
+		Linear: function(options)
+		{
+			var largest_fit = 0
+			
+			this.too_big = function(x) 
+			{
+				if (x - 1 === largest_fit)
+					return largest_fit
+					
+				return this.retry(x - 1)
+			}
+			
+			this.fits = function(x) 
+			{
+				largest_fit = x
+				return this.retry(x + 1)
+			}
+		},
+		
+		Binary: function(options)
+		{
+			var largest_fit
+			var minimum_too_big
+			
+			var step = options.Font_size_increment_step || 10
+		
+			this.too_big = function(x)
+			{
+				minimum_too_big = x
+				
+				if (largest_fit)
+				{
+					if (largest_fit === x - 1)
+						return largest_fit
+						
+					return this.retry(largest_fit + (x - largest_fit) / 2)
+				}
+				else
+				{
+					if (x === 1)
+						return 1
+						
+					return this.retry(x - step)
+				}
+			}
+			
+			this.fits = function(x)
+			{
+				largest_fit = x
+				
+				if (minimum_too_big)
+				{
+					if (minimum_too_big === x + 1)
+						return x
+						
+					return this.retry(x + (minimum_too_big - x) / 2)
+				}
+				else
+				{
+					return this.retry(x + step)
+				}
+			}
+		}
+	}
 
 	function get_initial_font_size(container)
 	{
@@ -57,95 +100,72 @@ $(function()
 	
 	function find_max_font_size(container, options)
 	{
-		var text = container.text()
+		container.css({ fontSize: 0 })
+			
+		var html = container.html()
 		var available_height = container.outerHeight()
 		var available_width = container.outerWidth()
 		
 		container.empty()
 		
-		var sandbox = $('<span/>').text(text).appendTo(container)
-		
-		var maximum_fitting_font_size
-		var minimum_too_big_font_size
-		
-		var step = Font_size_increment_step
-		if (options && options.Font_size_increment_step)
-			step = options.Font_size_increment_step
+		var sandbox = $('<span/>').html(html).appendTo(container)
 		
 		function try_font_size(font_size)
 		{
 			container.css({ fontSize: font_size + 'px' })
 		}
 		
-		function find_max_font_size_starting_with(font_size)
+		function recursive_search(algorythm, start_with)
 		{
-			font_size = Math.ceil(font_size)
-			if (font_size < 1)
-				font_size = 1
-				
-			try_font_size(font_size)
-			
-			var current_height = container[0].scrollHeight
-			var current_width = container[0].scrollWidth
-			
-			var height_proportion = current_height / available_height
-			var width_proportion = current_width / available_width
-			
-			if (height_proportion > 1 || width_proportion > 1)
+			var find_max_font_size_starting_with = function(font_size)
 			{
-				minimum_too_big_font_size = font_size
+				font_size = Math.ceil(font_size)
+				if (font_size < 1)
+					font_size = 1
+					
+				try_font_size(font_size)
 				
-				if (maximum_fitting_font_size)
-				{
-					if (maximum_fitting_font_size === font_size - 1)
-						return maximum_fitting_font_size
-						
-					return find_max_font_size_starting_with(maximum_fitting_font_size + (font_size - maximum_fitting_font_size) / 2)
-				}
-				else
-				{
-					if (font_size === 1)
-						return 1
-						
-					return find_max_font_size_starting_with(font_size - step)
-				}
-			}
-			else
-			{
-				maximum_fitting_font_size = font_size
+				var current_height = container[0].scrollHeight
+				var current_width = container[0].scrollWidth
 				
-				if (minimum_too_big_font_size)
-				{
-					if (minimum_too_big_font_size === font_size + 1)
-						return font_size
-						
-					return find_max_font_size_starting_with(font_size + (minimum_too_big_font_size - font_size) / 2)
-				}
+				var height_proportion = current_height / available_height
+				var width_proportion = current_width / available_width
+				
+				if (height_proportion > 1 || width_proportion > 1)
+					return algorythm.too_big(font_size)
 				else
-				{
-					return find_max_font_size_starting_with(font_size + step)
-				}
+					return algorythm.fits(font_size)
 			}
+			
+			algorythm.retry = find_max_font_size_starting_with
+			return find_max_font_size_starting_with(start_with)
 		}
 		
-		var font_size = find_max_font_size_starting_with(get_initial_font_size(container))
-		container.empty().text(text)
+		options.algorythm = options.algorythm || 'Binary'
+		var algorythm = new Algorythm[options.algorythm](options)
+		
+		var font_size = recursive_search(algorythm, get_initial_font_size(container))
+		container.empty().html(html)
 		return font_size
 	}
 	
 	$.fn.fill_with_text = function(options)
 	{
+		options = options || {}
+	
 		return $(this).each(function()
 		{
 			var container = $(this)
 			container.css({ fontSize: find_max_font_size(container, options) + 'px' })
 			
-			if (options)
-				if (options['center vertically'])
-				{
-					var label = $('<span/>').text(container.text()).appendTo(container.empty())
-					label.css({ position: 'relative', top: parseInt((container.height() - label.height()) / 2) + 'px' })
-				}
+			// my vertical centering hack
+			/*
+			if (options['center vertically'])
+			{
+				var label = $('<span/>').text(container.text()).appendTo(container.empty())
+				label.css({ position: 'relative', top: parseInt((container.height() - label.height()) / 2) + 'px' })
+			}
+			*/
 		})
 	}
 })(jQuery)
